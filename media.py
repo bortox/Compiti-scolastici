2871753ba95011e354f0f0e2dc82d6e90ce374aa
bortox@github.io <broto777@zoho.com>
2020-10-07 23:00:06 +0200

    gg

--- media.py
+++
@@ -1,6 +0,0 @@
-nalto = float(input('Inserisci il numero più alto'))
-nbasso = float(input('Inserisci il numero più basso'))
-result = f'({round((nbasso+nalto)/2,2)}±{round((nalto-nbasso)/2,2)})s'
-print(result)
-import clipboard
-clipboard.copy(result)
